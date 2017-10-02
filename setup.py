3456cb17e34448376857c98b4c552cedb1afdb2f
Wayne Keenan <wayne.keenan@gmail.com>
2017-10-02 06:22:41 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bleson',
+    version='0.0.1',
+    packages=['bleson'],
+    url='https://github.com/TheCellule/python-bleson',
+    license='MIT',
+    author='TheCellule',
+    author_email='thecellule@gmail.com',
+    description='Bluetooth LE Library'
+)
