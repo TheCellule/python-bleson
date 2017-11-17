bc19552e7bc9c49c577329dcd21067c0a1af8df8
Wayne Keenan <wayne@cannybots.com>
2017-11-17 17:12:16 +0000

    remove setuptool workarounds, fix for #2

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ print("Running on",system)
 
 if system == 'Windows':
     # Wheels are not yet supported by setuptools.
-    #install_requires.append('blesonwin')
-    import pip
-    pip.main(['install', 'blesonwin'])
+    install_requires.append('blesonwin')
+    #import pip
+    #pip.main(['install', 'blesonwin'])
 elif system == 'Darwin':
     # Wheels are not yet supported by setuptools.
-    # install_requires.append('pyobjc')
-    import pip
-    pip.main(['install', 'pyobjc'])
+    install_requires.append('pyobjc')
+    #import pip
+    #pip.main(['install', 'pyobjc'])
 
 
 
 setup(
     name='bleson',
-    version='0.0.5',
+    version='0.0.6',
     packages= find_packages(),
     install_requires=install_requires,
     url='https://github.com/TheCellule/python-bleson',
