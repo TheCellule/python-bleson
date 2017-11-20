268e7b5236d834c3ea40d3f94f7e4b4a2079c9fd
Wayne Keenan <wayne.keenan@gmail.com>
2017-11-20 05:55:14 +0000

    Added tox

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
+import sys, os
+import unittest
 from setuptools import setup, find_packages
+
+version='0.0.10'
+
+#pip setuptools wheel twine
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist")
+    os.system("twine upload dist/*")
+    sys.exit()
+
+
+def test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests')
+    return test_suite
 
 setup(
     name='bleson',
-    version='0.0.9',
+    version=version,
     packages= find_packages(),
     url='https://github.com/TheCellule/python-bleson',
     license='MIT',
@@ -16,5 +38,18 @@ setup(
         ':platform_system=="Darwin"': [
             'pyobjc'
         ]
-    }
+    },
+    test_suite='setup.test_suite',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Hardware",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
 )
