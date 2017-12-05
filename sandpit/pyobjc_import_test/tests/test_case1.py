6bf4a71a768b3424b462b1c12507af98ef8e5891
Wayne Keenan <wayne.keenan@gmail.com>
2017-12-05 19:23:14 +0000

    move sandpit to a branch

--- sandpit/pyobjc_import_test/tests/test_case1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import unittest
-import CoreBluetooth    # Comment out to fix 'objc.internal_error: Cannot initialize block support'
-
-class TestRoles(unittest.TestCase):
-    pass
